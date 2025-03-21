c2a49d6424d2be8bc49173d8b8da8bc3e71b664f
Subisha <subishasoman7@gmail.com>
2025-03-21 18:17:50 +0530

    metabase added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,7 @@ export const metadata: Metadata = {
     description: `Join us in celebrating the wedding reception of ${COUPLE_INFO.groom.name} and ${COUPLE_INFO.bride.name}. Together with their families, we invite you to share in our joy.`,
     images: ['/images/evening.jpeg']
   },
+  metadataBase: new URL('https://wedding-invitation-sigma-six.vercel.app'), // Replace with your actual domain
   alternates: {
     canonical: '/'
   }
